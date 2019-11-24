9d728f6f740b7ce13a3fb1dc7fc9a99b5637ce5e
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-11-24 22:10:06 +0600

    create some contact data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import Contact from './data/Contact';
 import Contacts from './data/Contacts';
+import ContactsComponent from './views/contact/ContactComponent'
 
+let data = [
+    {
+        name: 'Mushfiqur Rahman',
+        phone: '486755894u4',
+    },
+    {
+        name: 'Md Jamal Udding',
+        phone: '486755895876',
+    },
+    {
+        name: 'Md Tareq Mahmud',
+        phone: '486578568574',
+    }
+]
+
+let ul = ContactsComponent({
+    contactList: data
+})
+
+console.log(ul)
+
+let container = document.querySelector('.container');
+container.appendChild(ul)
