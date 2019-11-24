c4564ea707bafa56879eb3e5145986eccb2dd54d
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-11-24 22:10:34 +0600

    Create Contact view layer

---
+++ src/views/contact/ContactComponent.js
@@ -0,0 +1,19 @@
+import ContactItem from './contactItem'
+import contactItem from './contactItem';
+
+export default function ContactsComponent(props){
+    let ul = document.createElement('ul');
+    ul.classList.add('list-group');
+
+    props.contactList.forEach(contact => {
+        let item = contactItem({
+            name: contact.name,
+            phone: contact.phone,
+            avatar: contact.name.charAt(0).toUpperCase()
+        })
+
+        ul.appendChild(item)
+    })
+
+    return ul;
+}
---
+++ src/views/contact/contactItem.js
@@ -0,0 +1,28 @@
+import {create} from './../../halper'
+
+export default function contactItem(props){
+    
+    let li = document.createElement('li');
+    // li.className.add();
+    li.classList = 'list-group-item d-flex'
+
+    let avatar = document.createElement('div');
+    avatar.classList.add('avatar')
+    avatar.innerHTML = props.avatar;
+
+    let groupDiv = document.createElement('div');
+
+    let p1 = document.createElement('p');
+    p1.innerHTML = props.name;
+
+    let p2 = document.createElement('p');
+    p2.innerHTML = props.phone
+
+    groupDiv.append(p1);
+    groupDiv.append(p2);
+
+    li.append(avatar);
+    li.append(groupDiv);
+
+    return li;
+}
