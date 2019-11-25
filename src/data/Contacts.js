8d86e483ad8fec119742243d70727d16bca19825
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-11-25 21:13:31 +0600

    Remove constructor and back functional programming

--- src/data/Contacts.js
+++ src/data/Contacts.js
@@ -1,58 +1,45 @@
-class Contacts {
-    constructor(){
-        this.contactList = []
-    }
+const contactList = [];
 
-    //Create New Contact
-    createNewContact(contact){
-        this.contactList.push(contact);
-    }
-
-    //Get All Conctact
-    getContacts(){
-        return this.contactList;
-    }
-
-    //Find contact index number
-    getContactIndex(id){
-        //Find Contact index number for Delete and Update
-        let contactIndex = this.contactList.findIndex((contact) => {
-            return id === contact.id;
-        });
-    
-        return contactIndex;
-    }
-
-    //Get Single Contact
-    getSingleContact(){
-        let index = this.getContactIndex(id);
-    
-        return this.contactList[index];
-    }
-
-
-    //Delete Contact
-    deleteContact(contact){
-
-        let index = this.getContactIndex(contact.id)
-    
-        //Then Delete from ContactList Array.
-        this.contactList.splice(contactIndex, 1);
-    }
-
-    //Update Contact
-    updateConctact(oldContact, changeContact){
-        let index = this.getContactIndex(oldContact.id);
-    
-        for(let i in changeContact){
-            this.contactList[index][i] = changeContact[i];
-        };
-
-        // this.contactList[index] = {
-        //     ...this.contactList[index],
-        //     ...changeContact
-        // }
-    }
+export function createNewContact(contact){
+    contactList.push(contact);
 }
 
-export default Contacts;
+export function getContactList(){
+    return contactList;
+}
+
+function getContactIndex(id){
+    //Find Contact index number for Delete and Update
+    let contactIndex = contactList.findIndex((contact) => {
+        return id === contact.id;
+    });
+
+    return contactIndex;
+}
+
+export function getSingleContact(){
+    let index = getContactIndex(id);
+
+    return contactList[index];
+}
+
+export function deleteContact(contact){
+
+    let index = getContactIndex(contact.id)
+
+    //Then Delete from ContactList Array.
+    contactList.splice(contactIndex, 1);
+}
+
+export function updateConctact(oldContact, changeContact){
+    let index = getContactIndex(oldContact.id);
+
+    for(let i in changeContact){
+        contactList[index][i] = changeContact[i];
+    };
+
+    // this.contactList[index] = {
+    //     ...this.contactList[index],
+    //     ...changeContact
+    // }
+}
