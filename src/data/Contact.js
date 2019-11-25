35dabc6d9f1e670055039f9e5966d6da6f19b679
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-11-25 21:12:58 +0600

    Added favourite option

--- src/data/Contact.js
+++ src/data/Contact.js
@@ -1,14 +1,18 @@
 import uuid from 'uuid/v1'
 class Contact {
-    constructor({name = null, phone = null, email = null, fbLink = null, group = 'other'}){
-        this.id = uuid();
+    constructor({name = null, phone = null, email = null, fbLink = null, group = 'other', favourite = false}){
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.fbLink = fbLink;
         this.group = group;
+        this.favourite = favourite
+        this.id = uuid();
     }
 
+    print(){
+        console.log(this)
+    }
 
 }
 
